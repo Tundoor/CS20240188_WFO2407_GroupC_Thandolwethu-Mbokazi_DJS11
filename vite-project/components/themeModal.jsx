71fb03f73c9236d815763b361f2fd3925b503d67
Thando <thandwem045@gmail.com>
2024-11-29 00:04:51 +0200

    Added buttons to settings modal

--- vite-project/components/themeModal.jsx
+++ vite-project/components/themeModal.jsx
@@ -4,13 +4,24 @@ export default function SettingsModal({ closeModal }) {
     return (
         <div className="settingsDiv">
             <button className="closeModal" onClick={() => closeModal()}>X</button>
-            <div>    <h1 className="themes">Themes</h1>
-                <input className="divToggle" type="checkbox" />
-                <label className="DarkMode"></label>
-                <h1>Light Mode</h1>
-                <h1>Dark Mode</h1>
-                <h1 className="order">lexicographical order</h1>
+
+            <div className="Update">
+                <button className="mostUpdate"> Most Recent Update</button>
+                <button className="LeastUpdate">Least Recent Update</button>
             </div>
+
+            <div className="DarkMode">
+                <button className="Light">Light Mode</button>
+                <button className="Dark">Dark Mode</button>
+            </div>
+
+            <div className="orderDiv">
+                <h1 className="order">lexicographical order</h1>
+                <button className="A-Z">A-Z</button>
+                <button className="Z-A">Z-A</button>
+            </div>
+
         </div>
+
     )
 }
