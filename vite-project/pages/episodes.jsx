0a004c9fe98398cf73da27852454018ac4b2f3de
Thando <thandwem045@gmail.com>
2024-11-28 22:31:36 +0200

    Added Styling to  episode div

--- vite-project/pages/episodes.jsx
+++ vite-project/pages/episodes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 function Episodes() {
     const [episode, setEpisode] = useState([]);
@@ -60,19 +60,39 @@ function Episodes() {
 
     return (
         <div>
-            <h1>Episodes for {show.title}</h1>
-            <ul>
-                {episode.map((ep) => (
-                    <li key={ep.episode}>
-                        <h2>{ep.title}</h2>
-                        <p>{ep.description}</p>
-                        <audio controls>
+            <div className="showPodcastDiv">
+                <div className="podTitleDiv">
+                    <h1 className="podcastTitle">Episodes for {show.title}</h1>
+                </div>
+                {/* Links Back to home page */}
+                <Link to="/">
+                    <div className="podHome">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="45" height="45" viewBox="0 0 24 24">
+                            <path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 10 21 L 10 14 L 14 14 L 14 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z"></path>
+                        </svg>
+                    </div>
+                </Link>
+            </div>
+
+            <div className='epCard'>
+                {episode.map((ep, index) => (
+                    <>
+                        <div className="card" key={ep.id}>
+                            <img className="card-image" src={data.image}></img>
+                            <div className="card-info">
+                                <h1 className="card-title">{ep.title}</h1>
+                                <h6 className="card-episodes">Ep {index + 1}</h6>
+                                <h2 className="desc-title">Description</h2>
+                                <p className="card-desc">{ep.description}</p>
+                            </div>
+                        </div>
+                        {/* <audio controls>
                             <source src={ep.file} type="audio/mp3" />
                             Your browser does not support the audio element.
-                        </audio>
-                    </li>
+                        </audio> */}
+                    </>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
