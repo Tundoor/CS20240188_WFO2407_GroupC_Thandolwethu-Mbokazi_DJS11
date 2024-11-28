972bc83a72b86d1579f508a131e86bba11af1981
Thando <thandwem045@gmail.com>
2024-11-28 16:31:42 +0200

    Created placeholder for favourites

--- vite-project/pages/favourites.jsx
+++ vite-project/pages/favourites.jsx
@@ -1,6 +1,32 @@
 function Favourites() {
     return (
-        <h1>Favourites is a go </h1>
+        <div className="home-div">
+            <div className="logo-div">
+                <h1 className="logo">PodSphere</h1>
+                <p>Where Every Story Finds A Listener</p>
+            </div>
+            <div className="main-div">
+                <h1 className="main-header">Your Favourites</h1>
+                {/* Attempt to make this dynamic */}
+
+                <div className="main-cards">
+                    {/*  Renders our shows on the browser */}
+
+                    <div className="card" >
+                        <img className="card-image" src=""></img>
+                        <div className="card-info">
+                            <h1 className="card-title">Placeholder</h1>
+                            <h6 className="card-seasons">Seasons: </h6>
+                            <h6 className="card-episodes">Episodes: </h6>
+                            <h6 className="card-episodes">Updated On: </h6>
+                            <h2 className="desc-title">Description</h2>
+                            <p className="card-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium laborum impedit laudantium dolores aliquid similique obcaecati illo, et nisi qui? Quia cumque blanditiis doloremque iure quis consequatur ratione, quasi dolorem?</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
